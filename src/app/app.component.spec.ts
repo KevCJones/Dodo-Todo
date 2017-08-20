1f70ab6fef5c96d5df8dec80a0feebc5520eb8a3
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-08-20 22:20:30 +0100

    app boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,29 @@
+import { Warehouse, WarehouseConfig, DRIVER_TYPE, NgxWarehouseModule } from 'ngx-warehouse';
+import { TaskStoreService } from './services/task-store/task-store.service';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
+const config: WarehouseConfig = {
+  driver: DRIVER_TYPE.LOCALSTORAGE,
+  name: 'Dodotodo_tests2',
+  version: 1.0,
+  storeName: 'dodo_tasks_tests', // Should be alphanumeric, with underscores.
+  description: 'Test version : Todo app you do do daily...'
+};
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule, NgxWarehouseModule.configureWarehouse(config)],
+      providers: [TaskStoreService, Warehouse],
       declarations: [
-        AppComponent
+        AppComponent,
+        TaskListComponent,
+        TaskComponent,
       ],
     }).compileComponents();
   }));
@@ -21,12 +38,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
