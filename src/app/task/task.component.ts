9b368bb2a31881af066b3b19c06ae1bb4338327c
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-08-20 14:55:07 +0100

    unify the task component accessor approach to get / set pairs

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,10 @@
 import { ITask } from './itask';
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
+/*
+  Task Component is as dumb as i can make it. It takes data from a source input, renders it
+  watches for editing, marking as done events and emits the new content via a change event
+*/
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -10,8 +14,8 @@ export class TaskComponent implements OnInit {
 
   @Input() task: ITask;
   @Output() change: EventEmitter<ITask> = new EventEmitter();
-  beforeEditing: string;
-  isEditing = false;
+  private beforeEditing: string;
+  private isEditing = false;
 
   constructor() {}
 
@@ -34,16 +38,18 @@ export class TaskComponent implements OnInit {
     return this.task.done;
   }
 
-  doneEditing() {
-    this.isEditing = false;
-    if (this.label !== this.beforeEditing) {
+  set editing(isEditing) {
+    this.isEditing = isEditing;
+    if (!isEditing && (this.label !== this.beforeEditing)) {
       this.change.emit(this.task);
+    } else if (isEditing) {
+      this.isEditing = true;
+    this.beforeEditing = this.label;
     }
   }
 
-  startEditing() {
-    this.isEditing = true;
-    this.beforeEditing = this.label;
+  get editing() {
+    return this.isEditing;
   }
 
 }
