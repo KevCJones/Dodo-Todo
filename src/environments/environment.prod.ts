d14ae20636b11cb06cb38de41a2b32342e67760e
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-08-21 04:21:29 +0100

    mirror prodction environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+import { DRIVER_TYPE } from 'ngx-warehouse';
 export const environment = {
-  production: true
+  production: true,
+  storageConfig: {
+    driver: DRIVER_TYPE.LOCALSTORAGE,
+    name: 'Dodotodo',
+    version: 1.0,
+    storeName: 'dodo_tasks', // Should be alphanumeric, with underscores.
+    description: 'Todo app you do do daily...'
+  }
 };
